23048e334ceb1f68f725177bd36795e839aae185
zhuger <543585543@qq.com>
2022-02-17 14:49:19 +0800

    thinkphp6.0验证码前后分离api实现

--- src/CaptchaApi.php
+++ src/CaptchaApi.php
@@ -124,7 +124,7 @@ class CaptchaApi
 			'md5'   => md5($key),
         ];
     }
-	
+
 	/**
 	 * 输出验证码并把验证码的值保存的缓存中
 	 * @access public
@@ -136,14 +136,78 @@ class CaptchaApi
 	{
 		$this->configure();
         $generator = $this->generate();
-		
-		$this->cache->set('captchaSMS.'.$phone, $generator['code'], $this->expire);
-		return [
-		    'key'   => $phone,
-			'code'   => $generator['code'],
-		];
+
+        // 图片宽(px)
+        $this->imageW || $this->imageW = $this->length * $this->fontSize * 1.5 + $this->length * $this->fontSize / 2;
+        // 图片高(px)
+        $this->imageH || $this->imageH = $this->fontSize * 2.5;
+        // 建立一幅 $this->imageW x $this->imageH 的图像
+        $this->im = imagecreate((int) $this->imageW, (int) $this->imageH);
+        // 设置背景
+        imagecolorallocate($this->im, $this->bg[0], $this->bg[1], $this->bg[2]);
+
+        // 验证码字体随机颜色
+        $this->color = imagecolorallocate($this->im, mt_rand(1, 150), mt_rand(1, 150), mt_rand(1, 150));
+
+        // 验证码使用随机字体
+        $ttfPath = __DIR__ . '/../assets/' . ($this->useZh ? 'zhttfs' : 'ttfs') . '/';
+
+        if (empty($this->fontttf)) {
+            $dir  = dir($ttfPath);
+            $ttfs = [];
+            while (false !== ($file = $dir->read())) {
+                if ('.' != $file[0] && substr($file, -4) == '.ttf') {
+                    $ttfs[] = $file;
+                }
+            }
+            $dir->close();
+            $this->fontttf = $ttfs[array_rand($ttfs)];
+        }
+
+        $fontttf = $ttfPath . $this->fontttf;
+
+        if ($this->useImgBg) {
+            $this->background();
+        }
+
+        if ($this->useNoise) {
+            // 绘杂点
+            $this->writeNoise();
+        }
+        if ($this->useCurve) {
+            // 绘干扰线
+            $this->writeCurve();
+        }
+
+        // 绘验证码
+        $text = $this->useZh ? preg_split('/(?<!^)(?!$)/u', $generator['value']) : str_split($generator['value']); // 验证码
+
+        foreach ($text as $index => $char) {
+
+            $x     = $this->fontSize * ($index + 1) * mt_rand(1.2, 1.6) * ($this->math ? 1 : 1.5);
+            $y     = $this->fontSize + mt_rand(10, 20);
+            $angle = $this->math ? 0 : mt_rand(-40, 40);
+
+            imagettftext($this->im, $this->fontSize, $angle, $x, $y, $this->color, $fontttf, $char);
+        }
+
+        ob_start();
+        // 输出图像
+        imagepng($this->im);
+        $content = ob_get_clean();
+        imagedestroy($this->im);
+
+        $this->cache->set('captchaSMS.'.$phone, $generator['code'], $this->expire);
+        // api调用
+        //if ($api) {
+            return [
+                'key'   => $phone,
+                'code'   => $generator['code'],
+                'img' => 'data:image/png;base64,' . chunk_split(base64_encode($content))
+            ];
+        //}
 	}
-	
+
 	/**
 	 * 输出验证码并把验证码的值保存的缓存中
 	 * @access public
@@ -157,11 +221,11 @@ class CaptchaApi
 		if($this->cache->get('captchaSMS.'.$phone)==$code){
 			$runres = true;
 		}
-		
+
 		if ($res || $runres) {
 		    $this->cache->delete('captchaSMS.'.$phone);
 		}
-		
+
 		return $runres;
 	}
 
